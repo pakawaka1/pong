a02c34d73710b24f5d37b7c52a2d9f8017f6dbc4
David Cervantes <davidcervantesdev@gmail.com>
2024-01-05 19:01:03 -0800

    feat:  added working sound!

--- pong.py
+++ pong.py
@@ -95,12 +95,12 @@ while True:
     if ball.ycor() > 290:
         ball.sety(290)
         ball.dy *= -1
-        os.system = ("afplay bounce.wav&")
+        os.system("afplay bounce.wav&")
 
     if ball.ycor() < -290:
         ball.sety(-290)
         ball.dy *= -1
-        os.system = ("afplay bounce.wav&")
+        os.system("afplay bounce.wav&")
 
 
     if ball.xcor() > 390:
@@ -123,10 +123,10 @@ while True:
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
-        os.system = ("afplay bounce.wav&")
+        os.system("afplay bounce.wav&")
 
 
     if (ball.xcor() < -340 and ball.xcor() > - 350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
-        os.system = ("afplay bounce.wav&")
+        os.system("afplay bounce.wav&")
