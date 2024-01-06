fbefd0e50c7b2f3a7931ecbf6f32544a54b125d2
David Cervantes <davidcervantesdev@gmail.com>
2024-01-05 17:29:33 -0800

    feat:  added border checking so ball stays in screen

--- pong.py
+++ pong.py
@@ -33,6 +33,12 @@ ball.shape("square")
 ball.color("white")
 ball.penup()
 ball.goto(0, 0)
+#change ball direction and speed ()
+ball.dx = 2
+ball.dy = 2
+
+
+
 
 # function
 def paddle_a_up():
@@ -68,3 +74,24 @@ wn.onkeypress(paddle_b_down, "Down")
 # Main Game Loop
 while True:
     wn.update()
+
+    # move the ball
+    ball.setx(ball.xcor() + ball.dx)
+    ball.sety(ball.ycor() + ball.dy)
+
+    #border checking
+    if ball.ycor() > 290:
+        ball.sety(290)
+        ball.dy *= -1
+
+    if ball.ycor() < -290:
+        ball.sety(-290)
+        ball.dy *= -1
+
+    if ball.xcor() > 390:
+        ball.goto(0,0)
+        ball.dx *= -1
+
+    if ball.xcor() < -390:
+        ball.goto(0,0)
+        ball.dx *= -1
