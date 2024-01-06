526346f25051cfd3feadeaf96357484b61702986
David Cervantes <davidcervantesdev@gmail.com>
2024-01-05 18:03:01 -0800

    feat:  added scoring updates

--- pong.py
+++ pong.py
@@ -7,6 +7,10 @@ wn.bgcolor("black")
 wn.setup(width=800, height=600)
 wn.tracer(0)
 
+# keeping score
+score_a = 0
+score_b = 0
+
 # Paddle A
 paddle_a = turtle.Turtle()
 paddle_a.speed(0)
@@ -37,6 +41,15 @@ ball.goto(0, 0)
 ball.dx = 1
 ball.dy = 1
 
+# pen (scorekeeping)
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0 Player B : 0", align="center", font=("Courier", 24, "normal"))
+ 
 
 
 
@@ -91,10 +104,17 @@ while True:
     if ball.xcor() > 390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {} Player B : {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     if ball.xcor() < -390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {} Player B : {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
+
 
     #paddle and ball collisions
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
